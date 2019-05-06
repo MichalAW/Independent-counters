5102dc1d815bdfaff2f60ac2085af17f91c2a3d8
MichalAW <michal.wanta@gmail.com>
2019-05-06 22:43:43 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -5,23 +5,6 @@ var Counter = React.createClass({
         };
     },
 
-    render: function () {
-        return React.createElement('div', {},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
-        );
-    }
-});
-
-var element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('app'));
-
-var Counter = React.createClass({
-    getInitialState: function () {
-        return {
-            counter: 0
-        };
-    },
-
     increment: function () {
         this.setState({
             counter: this.state.counter + 1
@@ -37,9 +20,9 @@ var Counter = React.createClass({
     }
 });
 
-render: function () {
+render: function() {
     return (
-        // zauważ, że metoda zwraca jeden element!
+        // This method returns only one element!
         React.createElement('div', {
                 className: 'contactItem'
             },
@@ -62,3 +45,35 @@ render: function () {
         )
     )
 }
+//my own pre-work:
+var Increment = React.createClass({
+    // With createReactClass(), provide a separate getInitialState method that returns the initial state:
+    getInitialState: function () {
+        return {
+            counter: 0
+        };
+    },
+    // Add one
+    increment: function () {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+    // Minus one
+    decrement: function () {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+    // Create div and buttons    
+    render() {
+        return React.createElement('div', {id:'counter', this.state.counter},
+            React.createElement('button', {this.increcement}, 'Add 1'),
+            React.createElement('button', {this.decrement}, 'Minus 1')
+        )
+    }
+});
+// Render elements
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+React.render( < Increment / > , document.getElementById('container'));
